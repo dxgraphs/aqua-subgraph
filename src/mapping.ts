ce5d6ce7d0c7514e4431b4267bd45f657cc6dfa2
Adam Azad <adam@adamazad.com>
2021-02-23 19:04:05 +0300

    Add getAuctionToken

--- src/mapping.ts
+++ src/mapping.ts
@@ -32,14 +32,14 @@ export function handleNewAuction(event: NewAuction): void {
   auction.gracePeriod = 0 //ToDo: Add to contract event
   auction.tokenAmount = event.params._auctionedSellAmount.toI32()
 
-  let tokenIn = new AuctionToken(event.params._biddingToken.toHexString())
+  let tokenIn = getAuctionToken(event.params._biddingToken.toHexString())
   tokenIn.name = fetchTokenName(event.params._biddingToken)
   tokenIn.address = event.params._biddingToken.toString()
   tokenIn.symbol = fetchTokenSymbol(event.params._biddingToken)
   tokenIn.decimals = fetchTokenDecimals(event.params._biddingToken).toI32()
   // ToDo: tokenIn.icon = ''
 
-  let tokenOut = new AuctionToken(event.params._auctioningToken.toHexString())
+  let tokenOut = getAuctionToken(event.params._auctioningToken.toHexString())
 
   tokenIn.name = fetchTokenName(event.params._auctioningToken)
   tokenIn.address = event.params._auctioningToken.toString()
@@ -89,4 +89,21 @@ export function handleNewSellOrder(event: NewSellOrder): void {
   bid.address = event.transaction.from.toHexString()
   bid.status = BID_STATUS.SUBMITTED
   bid.save()
+}
+
+/**
+ * Returns an existing AuctionToken instance if exists.
+ * Creates a new instance if it does not
+ * @param tokenContractAddress
+ */
+export function getAuctionToken(tokenContractAddress: string): AuctionToken {
+  // Try to fetch existing token
+  let auctionToken = AuctionToken.load(tokenContractAddress)
+
+  // Token does not exist, create new record
+  if (auctionToken == null) {
+    auctionToken = new AuctionToken(tokenContractAddress)
+  }
+
+  return auctionToken
 }
