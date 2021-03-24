b491326ca14c7a57a7aaf78db2a1217ff4337bd2
Adam Azad <adam@adamazad.com>
2021-03-24 12:00:01 +0300

    Add handlers for TemplateLauncher

--- src/mappings/templates.ts
+++ src/mappings/templates.ts
@@ -1,5 +1,5 @@
 // Contract ABIs and Events
-import { TemplateAdded } from '../../generated/TemplateLauncher/TemplateLauncher'
+import { TemplateAdded, TemplateRemoved, TemplateVerified } from '../../generated/TemplateLauncher/TemplateLauncher'
 
 // Helpers
 import { fetchTemplateName } from './helpers'
@@ -7,6 +7,10 @@ import { fetchTemplateName } from './helpers'
 // GraphQL Schemas
 import { AuctionTemplate } from '../../generated/schema'
 
+/**
+ * Handles adding a new template
+ * @param event the `TemplateAdded` event
+ */
 export function handleTemplateAdded(event: TemplateAdded): void {
   let auctionTemplate = new AuctionTemplate(event.params.templateId.toHexString())
   // Factory address
@@ -15,7 +19,27 @@ export function handleTemplateAdded(event: TemplateAdded): void {
   auctionTemplate.address = event.params.template.toHexString()
   // Auction name: used to resolve ID to contract ABIs
   auctionTemplate.name = fetchTemplateName(event.address)
-
+  // By default, templates are unverified
+  auctionTemplate.verified = false
   // Save entity
   auctionTemplate.save()
 }
+
+/**
+ * Handles adding a new template
+ * @param event the `TemplateAdded` event
+ */
+export function handleTemplateVerified(event: TemplateVerified): void {
+  let auctionTemplate = AuctionTemplate.load(event.params.templateId.toHexString())
+  if (auctionTemplate) {
+    auctionTemplate.verified = true
+    // Save entity
+    auctionTemplate.save()
+  }
+}
+
+/**
+ * Handles removing templates from the contract
+ * @param event
+ */
+export function handleTemplateRemved(event: TemplateRemoved): void {}
