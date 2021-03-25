a8f9a7a337bc95dfc24833c25b1913f2ce486ec4
Adam Azad <adam@adamazad.com>
2021-03-26 02:15:39 +0300

    Update factory handlers and tests

--- src/mappings/factory.ts
+++ src/mappings/factory.ts
@@ -1,21 +1,11 @@
-import { log } from '@graphprotocol/graph-ts'
-
 // Contract ABIs and Events
-import { FactoryInitialized, TemplateLaunched } from '../../generated/MesaFactory/MesaFactory'
-import { EasyAuction as EasyAuctionContract } from '../../generated/EasyAuction/EasyAuction'
+import { FactoryInitialized } from '../../generated/MesaFactory/MesaFactory'
 
 // GraphQL Schemas
 import * as Schemas from '../../generated/schema'
 
 // Mapping helpers
-import {
-  AUCTION_STATUS,
-  fetchTokenDecimals,
-  fetchTokenName,
-  fetchTokenSymbol,
-  getOrCreateAuctionToken,
-  MESA_FACTORY
-} from './helpers'
+import { MESA_FACTORY } from './helpers'
 
 /**
  * Handle initilizing the MesaFactory.
@@ -34,112 +24,12 @@ export function handleFactoryInitialized(event: FactoryInitialized): void {
   // Fee Manager
   mesaFactory.feeManager = event.params.feeManager.toHexString()
   mesaFactory.feeNumerator = event.params.feeNumerator.toI32()
-
+  // Auction count
+  mesaFactory.auctionCount = 0
   // Address of TemplateLauncher contract
   mesaFactory.templateLauncher = event.params.templateLauncher.toHexString()
   // Address of TemplateManager contract
   mesaFactory.templateManager = event.params.templateManager.toHexString()
   // Save
   mesaFactory.save()
-}
-
-/**
- * Handles launching a new Auction - EasyAuction or FixedPriceAuction from AuctionLauncher via MesaFactory.
- *
- * See [`MesaFactory.LuanchTemplate`](https://github.com/cryptonative-ch/mesa-smartcontracts/blob/main/contracts/MesaFactory.sol)
- *
- * @todo Load the AuctionName from the contract to determine type of Auction
- *
- */
-export function handleTemplateLaunched(event: TemplateLaunched): void {
-  log.info('New template launched addres: {} {}', [event.params.auction.toString(), event.params.templateId.toString()])
-
-  // Determine the entity to use from the templateId
-  const auctionTemplate = Schemas.AuctionTemplate.load(event.params.templateId.toHex())
-
-  // Early exit
-  if (!auctionTemplate) {
-    return
-  }
-
-  // EasyAuction
-  if (auctionTemplate.name === 'EasyAuction') {
-    createEasyAuction(event)
-  }
-
-  // FixedPriceAuction
-  if (auctionTemplate.name === 'FixedPriceAuction') {
-    createFixedPriceAuction(event)
-  }
-
-  // Add more templates to registery and
-}
-
-/**
- * Creates a new EasyAuction entity
- * @param event `TemplateLaunched` event
- */
-function createEasyAuction(event: TemplateLaunched): void {
-  // Bind the new Contract
-  let easyAuctionContract = EasyAuctionContract.bind(event.params.auction)
-
-  // Assume that the first template registered is EasyAuction
-  let easyAuction = new Schemas.EasyAuction(event.params.auction.toHexString())
-  // Timestamps
-  easyAuction.createdAt = event.block.timestamp.toI32()
-  easyAuction.updatedAt = event.block.timestamp.toI32()
-
-  easyAuction.minimumBidAmount = easyAuctionContract.minimumBiddingAmountPerOrder().toI32()
-  // Save initial data
-  easyAuction.save()
-  // Entity
-  let auction = new Schemas.EasyAuction(event.address.toHexString())
-
-  // Start and end dates of the auction
-  auction.startDate = easyAuctionContract.auctionStartedDate().toI32()
-  auction.endDate = easyAuctionContract.auctionEndDate().toI32()
-  // Grace period start and end
-  auction.gracePeriodStartDate = easyAuctionContract.gracePeriodEndDate().toI32()
-  auction.gracePeriodEndDate = easyAuctionContract.gracePeriodEndDate().toI32()
-
-  // Auction status
-  auction.status = AUCTION_STATUS.UPCOMING
-
-  // Total amount of token to be auctioned
-  // auction.tokenAmount = ?
-
-  // Bidding token / token in
-  let tokenIn = getOrCreateAuctionToken(easyAuctionContract.biddingToken())
-  tokenIn.name = fetchTokenName(easyAuctionContract.biddingToken())
-  tokenIn.symbol = fetchTokenSymbol(easyAuctionContract.biddingToken())
-  tokenIn.decimals = fetchTokenDecimals(easyAuctionContract.biddingToken()).toI32()
-
-  // Auctioning token / token out
-  let tokenOut = getOrCreateAuctionToken(easyAuctionContract.auctioningToken())
-  tokenOut.name = fetchTokenName(easyAuctionContract.auctioningToken())
-  tokenOut.symbol = fetchTokenSymbol(easyAuctionContract.auctioningToken())
-  tokenOut.decimals = fetchTokenDecimals(easyAuctionContract.auctioningToken()).toI32()
-
-  // Update assign tokenIn and TokenOut foregin keys
-  auction.tokenIn = tokenIn.id
-  auction.tokenOut = tokenOut.id
-
-  /**
-   * @todo find a resolver for the name
-   * Use the token's name for name of the Auction
-   */
-  auction.name = tokenOut.name
-
-  // Save everything
-  tokenIn.save()
-  tokenOut.save()
-  auction.save()
-}
-
-/**
- * Creates a new FixedPriceAuction entity
- * @param event `TemplateLaunched` event
- */
-function createFixedPriceAuction(event: TemplateLaunched): void {
-  // WIP
 }
---
+++ tests/factory.spec.ts
@@ -0,0 +1,107 @@
+// Externals
+import { providers } from 'ethers'
+import Axios from 'axios'
+
+// Contracts
+import { AuctionLauncher, MesaFactory, ERC20, TemplateLauncher } from './helpers/contracts'
+
+// Helpers
+import { buildSubgraphYaml, EVM_ENDPOINT, execAsync, getContractFactory, GRAPHQL_ENDPOINT, wait } from './helpers'
+
+describe('MesaFactory', () => {
+  // Connect to local ganache instance
+  const provider = new providers.JsonRpcProvider(EVM_ENDPOINT)
+
+  // Wallets/Signers
+  const deployer = provider.getSigner(0)
+
+  // Contracts
+  let mesaFactory: MesaFactory
+  let auctionLauncher: AuctionLauncher
+  let templateLauncher: TemplateLauncher
+
+  // The Graph client
+  const theGraph = Axios.create({
+    baseURL: GRAPHQL_ENDPOINT,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  beforeEach(async () => {
+    // Wait for everything to fire up
+    await wait(10000)
+
+    // Before each unit test, a new MesaFactory, TemplateLauncher, and AuctionLauncher EasyAuction contract is deployed to ganache
+    // then followed by deploying its subgraph to the Graph node
+
+    // Deploy MesaFactory
+    mesaFactory = (await getContractFactory('MesaFactory', deployer).deploy()) as MesaFactory
+    // Deploy TemplateLauncher
+    templateLauncher = (await getContractFactory('TemplateLauncher', deployer).deploy(
+      mesaFactory.address
+    )) as TemplateLauncher
+    // Deploy AuctionLauncher
+    auctionLauncher = (await getContractFactory('AuctionLauncher', deployer).deploy(
+      mesaFactory.address
+    )) as AuctionLauncher
+
+    // Prepare subgraph.yaml
+    await buildSubgraphYaml({
+      network: 'local',
+      startBlock: mesaFactory.deployTransaction.blockNumber as number,
+      contracts: {
+        factory: {
+          address: mesaFactory.address
+        },
+        auctionLauncher: {
+          address: auctionLauncher.address
+        },
+        templateLauncher: {
+          address: templateLauncher.address
+        }
+      }
+    })
+
+    // Initilize the Factory
+    await mesaFactory.initalize(
+      await deployer.getAddress(),
+      await deployer.getAddress(),
+      await deployer.getAddress(),
+      templateLauncher.address,
+      0, // zero fees
+      0 // zero fees
+    )
+
+    // Build, create and deploy the subgraph
+    await execAsync('npm run build')
+    await execAsync('npm run create-local')
+    await execAsync('npm run deploy-local')
+
+    // Wait for subgraph to sync
+    await wait(20000)
+  })
+
+  afterEach(async () => {
+    // Clean up
+    await execAsync('npm run remove-local')
+  })
+
+  test('Should return MesaFactory', async () => {
+    const { data, status } = await theGraph.post(GRAPHQL_ENDPOINT, {
+      query: `{
+          mesaFactory (id: "MesaFactory") {
+            address
+            feeManager
+            templateLauncher
+          }
+        }
+      `
+    })
+
+    expect(status).toBe(200)
+    expect(data.data.mesaFactory.address).toEqual(mesaFactory.address)
+    expect(data.data.mesaFactory.feeManager).toEqual(await mesaFactory.feeManager())
+    expect(data.data.mesaFactory.templateLauncher).toEqual(await mesaFactory.templateLauncher())
+  })
+})
