c013a6271a53bad4bbbbbc96a6f9d75f1d076770
Adam Azad <adam@adamazad.com>
2021-03-30 00:37:11 +0300

    Add populate-graph.ts
    For testing purposes

---
+++ pupluate-graph.ts
@@ -0,0 +1,141 @@
+// Externals
+import { ethers, providers } from 'ethers'
+import { start as StartREPL } from 'repl'
+
+// Contracts
+import { AuctionLauncher, EasyAuction, ERC20, MesaFactory, TemplateLauncher } from './tests/helpers/contracts'
+
+// Helpers
+import { buildSubgraphYaml, EVM_ENDPOINT, execAsync, getContractFactory, GRAPHQL_ENDPOINT, wait } from './tests/helpers'
+;(async () => {
+  // Allow use to access contracts
+  const replInstance = StartREPL({
+    preview: true,
+    useColors: true,
+    prompt: `Mesa > `
+  })
+
+  console.log(`Starting Docker`)
+
+  await execAsync('npm run docker-up')
+
+  // Wait for everything to fire up
+  await wait(10000)
+
+  // Connect to local ganache instance
+  const provider = new providers.JsonRpcProvider(EVM_ENDPOINT)
+
+  // Wallets/Signers
+  const deployer = provider.getSigner(0)
+
+  console.log(`Using ${await deployer.getAddress()} as deployer for contracts`)
+
+  // Before each unit test, a new MesaFactory, TemplateLauncher, and AuctionLauncher EasyAuction contract is deployed to ganache
+  // then followed by deploying its subgraph to the Graph node
+
+  const weth = (await getContractFactory('ERC20Mintable', deployer).deploy('WETH', 'WETH')) as ERC20
+
+  // Deploy MesaFactory
+  const mesaFactory = (await getContractFactory('MesaFactory', deployer).deploy()) as MesaFactory
+  // Deploy TemplateLauncher
+  const templateLauncher = (await getContractFactory('TemplateLauncher', deployer).deploy(
+    mesaFactory.address
+  )) as TemplateLauncher
+  // Deploy AuctionLauncher
+  const auctionLauncher = (await getContractFactory('AuctionLauncher', deployer).deploy(
+    mesaFactory.address
+  )) as AuctionLauncher
+
+  const buildSubgraphYamlConfig = {
+    network: 'local',
+    startBlock: mesaFactory.deployTransaction.blockNumber as number,
+    contracts: {
+      factory: {
+        address: mesaFactory.address
+      },
+      auctionLauncher: {
+        address: auctionLauncher.address
+      },
+      templateLauncher: {
+        address: templateLauncher.address
+      }
+    }
+  }
+
+  console.log(`Building subgraph.yaml using: `, buildSubgraphYamlConfig)
+  // Prepare subgraph.yaml
+  await buildSubgraphYaml(buildSubgraphYamlConfig)
+
+  // Build, create and deploy the subgraph
+  console.log('Bulding subgraph')
+  await execAsync('npm run build')
+
+  console.log('Creating subgraph')
+  await execAsync('npm run create-local')
+
+  await wait(4000)
+
+  console.log('Deploying subgraph')
+  await execAsync('npm run deploy-local')
+
+  // Wait for subgraph to sync
+  await wait(20000)
+
+  console.log('Initlizing the MesaFactory')
+  // Initilize the Factory
+  const mesaFactoryInitalizeTx = await mesaFactory.initalize(
+    await deployer.getAddress(),
+    await deployer.getAddress(),
+    await deployer.getAddress(),
+    templateLauncher.address,
+    0, // zero fees
+    0 // zero fees
+  )
+  console.log('Factory initialized')
+
+  console.log('Deploying an EasyAuction Template contract')
+  // Create factory for EasyAuction and deploy a new version
+  const easyAuction = (await getContractFactory('EasyAuction', deployer).deploy()) as EasyAuction
+
+  console.log('Register EasyAuction in AuctionLauncher')
+  // Register EasyAuction in AuctionLauncher
+  const auctionLaunch1 = await auctionLauncher.addTemplate(easyAuction.address)
+  const auctionLaunch1Receipt = await auctionLaunch1.wait(1)
+  // Get the EasyAuction TemplateId
+  const easyAuctionTemplateId = auctionLaunch1Receipt.events?.[0]?.args?.templateId
+
+  const easyAuctionTemplate = await getContractFactory('EasyAuctionTemplate', deployer).deploy(
+    weth.address,
+    auctionLauncher.address,
+    easyAuctionTemplateId
+  )
+
+  // Register EasyAuctionTemplate on TemplateLauncher
+  await templateLauncher.addTemplate(easyAuctionTemplate.address)
+
+  // await mesaFactory.launchTemplate(easyAuctionTemplateId, encodeInitData(
+
+  // ))
+
+  replInstance.context.mesaFactory = mesaFactory
+  replInstance.context.templateLauncher = templateLauncher
+  replInstance.context.auctionLauncher = auctionLauncher
+  replInstance.context.helpers = require('./tests/helpers')
+
+  console.log(`Subgraph ready at ${GRAPHQL_ENDPOINT}`)
+})()
+
+export function encodeInitData(
+  tokenOut: string,
+  tokenIn: string,
+  duration: string,
+  tokenOutSupply: string,
+  minPrice: string,
+  minBuyAmount: string,
+  minRaise: string
+) {
+  return ethers.utils.defaultAbiCoder.encode(
+    ['address', 'address', 'uint256', 'uint256', 'uint96', 'uint96', 'uint256'],
+    [tokenOut, tokenIn, duration, tokenOutSupply, minPrice, minBuyAmount, minRaise]
+  )
+}
