20086f7d536cf958e83526f7a8b79c954ae0c3b4
Adam Azad <adam@adamazad.com>
2021-04-11 23:11:34 +0300

    Add mapping for rest of MesaFactory events

--- src/mappings/factory.ts
+++ src/mappings/factory.ts
@@ -1,5 +1,15 @@
 // Contract ABIs and Events
-import { FactoryInitialized, MesaFactory as MesaFactoryContract } from '../../generated/MesaFactory/MesaFactory'
+import {
+  FactoryInitialized,
+  SetFeeManager,
+  SetFeeNumerator,
+  SetFeeTo,
+  SetSaleFee,
+  SetTemplateManager,
+  MesaFactory as MesaFactoryContract,
+  SetTemplateLauncher,
+  SetTemplateFee
+} from '../../generated/MesaFactory/MesaFactory'
 
 // GraphQL Schemas
 import * as Schemas from '../../generated/schema'
@@ -24,6 +34,7 @@ export function handleFactoryInitialized(event: FactoryInitialized): void {
   // Fees
   mesaFactory.feeManager = event.params.feeManager.toHexString()
   mesaFactory.feeNumerator = event.params.feeNumerator.toI32()
+  mesaFactory.templateFee = event.params.templateFee.toI32()
   // Auction count
   mesaFactory.saleCount = 0
   // Address of TemplateLauncher contract
@@ -31,5 +42,82 @@ export function handleFactoryInitialized(event: FactoryInitialized): void {
   // Address of TemplateManager contract
   mesaFactory.templateManager = event.params.templateManager.toHexString()
   // Save
+  mesaFactory.save()
+}
+
+export function handleSetFeeManager(event: SetFeeManager): void {
+  let mesaFactory = Schemas.MesaFactory.load(MESA_FACTORY.ID)
+
+  if (!mesaFactory) {
+    return
+  }
+
+  mesaFactory.feeManager = event.params.feeManager.toString()
+  mesaFactory.save()
+}
+
+export function handleSetFeeNumerator(event: SetFeeNumerator): void {
+  let mesaFactory = Schemas.MesaFactory.load(MESA_FACTORY.ID)
+
+  if (!mesaFactory) {
+    return
+  }
+
+  mesaFactory.feeNumerator = event.params.feeNumerator
+  mesaFactory.save()
+}
+
+export function handleSetFeeTo(event: SetFeeTo): void {
+  let mesaFactory = Schemas.MesaFactory.load(MESA_FACTORY.ID)
+
+  if (!mesaFactory) {
+    return
+  }
+
+  mesaFactory.feeTo = event.params.feeTo.toString()
+  mesaFactory.save()
+}
+
+export function handleSetSaleFee(event: SetSaleFee): void {
+  let mesaFactory = Schemas.MesaFactory.load(MESA_FACTORY.ID)
+
+  if (!mesaFactory) {
+    return
+  }
+
+  mesaFactory.saleFee = event.params.saleFee
+  mesaFactory.save()
+}
+
+export function handleSetTemplateFee(event: SetTemplateFee): void {
+  let mesaFactory = Schemas.MesaFactory.load(MESA_FACTORY.ID)
+
+  if (!mesaFactory) {
+    return
+  }
+
+  mesaFactory.temp = event.params.templateFee
+  mesaFactory.save()
+}
+
+export function handleSetTemplateManager(event: SetTemplateManager): void {
+  let mesaFactory = Schemas.MesaFactory.load(MESA_FACTORY.ID)
+
+  if (!mesaFactory) {
+    return
+  }
+
+  mesaFactory.templateManager = event.params.templateManager.toString()
+  mesaFactory.save()
+}
+
+export function handleSetTemplateLauncher(event: SetTemplateLauncher) {
+  let mesaFactory = Schemas.MesaFactory.load(MESA_FACTORY.ID)
+
+  if (!mesaFactory) {
+    return
+  }
+
+  mesaFactory.templateLauncher = event.params.templateLauncher.toString()
   mesaFactory.save()
 }
