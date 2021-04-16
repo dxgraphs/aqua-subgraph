9f0a7632400c9104354a46baa97f2d21e2ff43e4
Adam Azad <adam@adamazad.com>
2021-04-16 20:38:21 +0300

    Add SaleLauncher tests

---
+++ tests/saleLauncher.spec.ts
@@ -0,0 +1,116 @@
+// Externals
+import { utils } from 'ethers'
+// Signers
+import { createTokenAndMintAndApprove, getContractFactory, SUBGRAPH_SYNC_SECONDS, wait, getSigners } from './helpers'
+import { mesaJestAfterEach, mesaJestBeforeEach, MesaJestBeforeEachContext } from '../jest/setup'
+import { FixedPriceSale, FixedPriceSaleTemplate, FixedPriceSale__factory } from './helpers/contracts'
+import { createFixedPriceSale } from '../scripts/helpers'
+
+// Test block
+describe('SaleLauncher', function() {
+  let mesa: MesaJestBeforeEachContext
+
+  beforeEach(async () => {
+    mesa = await mesaJestBeforeEach()
+  })
+
+  afterEach(async () => {
+    await mesaJestAfterEach()
+  })
+
+  test('Should save new FixedPriceSale', async () => {
+    const [deployer, saleCreator, saleInvestorA, saleInvestorB] = getSigners(mesa.provider)
+    // Register sale
+    const fixedPriceSale = (await getContractFactory('FixedPriceSale', deployer).deploy()) as FixedPriceSale
+    // Register FixedPriceSale in SaleLauncher
+    const fixedPriceSaleTemplate = (await getContractFactory(
+      'FixedPriceSaleTemplate',
+      mesa.provider.getSigner(0)
+    ).deploy()) as FixedPriceSaleTemplate
+    // Register sale in SaleLauncher
+    const txReceipt1 = await (await mesa.saleLauncher.addTemplate(fixedPriceSale.address)).wait(1)
+    // Regiter template in TemplateLauncher
+    const txReceipt2 = await (await mesa.templateLauncher.addTemplate(fixedPriceSaleTemplate.address)).wait(1)
+    // Comment if you wish to debug
+    // console.log(txReceipt1.events)
+    // console.log(txReceipt2.events)
+    // Deploy, mint and approve Auctioning Token
+    const fixedPriceSaleToken = await createTokenAndMintAndApprove({
+      name: 'Fixed Price Sale Token',
+      symbol: 'FPST',
+      addressToApprove: mesa.saleLauncher.address,
+      numberOfTokens: utils.parseEther('1000'),
+      users: [saleCreator],
+      signer: saleCreator
+    })
+
+    // Deploy, mint and approve Bidding Token
+    const biddingToken = await createTokenAndMintAndApprove({
+      name: 'Bidding Token',
+      symbol: 'BT',
+      addressToApprove: mesa.saleLauncher.address,
+      numberOfTokens: utils.parseEther('1000'),
+      users: [saleCreator, saleInvestorA, saleInvestorB],
+      signer: deployer
+    })
+
+    // Launch FixedPriceSale
+    const newFixedPriceSaleAddress = await createFixedPriceSale({
+      templateId: 1,
+      mesaFactory: mesa.mesaFactory,
+      saleLauncher: mesa.saleLauncher,
+      biddingToken: biddingToken,
+      saleToken: fixedPriceSaleToken,
+      saleCreator
+    })
+    console.log(`Launched a new FixedPriceSale at ${newFixedPriceSaleAddress}`)
+    const launchedfixedPriceSale = FixedPriceSale__factory.connect(newFixedPriceSaleAddress, saleCreator)
+
+    console.log((await launchedfixedPriceSale.tokensForSale()).toString())
+    await wait(SUBGRAPH_SYNC_SECONDS * 5)
+
+    const { data } = await mesa.fetchFromTheGraph(`{
+      fixedPriceSale (id: "${newFixedPriceSaleAddress}") {
+          id
+          status
+          sellAmount
+          startDate
+          endDate
+          minimumRaise
+          allocationMin
+          allocationMax
+          tokenIn {
+            id
+            name
+            symbol
+            decimals
+          }
+          tokenOut {
+            id
+            name
+            symbol
+            decimals
+          }
+          purchases {
+            id
+            amount
+            buyer
+          }
+      }
+    }`)
+
+    expect(data.data.fixedPriceSale.id).toMatch(newFixedPriceSaleAddress)
+    expect(data.data.fixedPriceSale.status).toMatch('upcoming')
+    expect(data.data.fixedPriceSale.minimumRaise).toMatch((await launchedfixedPriceSale.minimumRaise()).toString())
+    expect(data.data.fixedPriceSale.allocationMin).toMatch((await launchedfixedPriceSale.allocationMin()).toString())
+    expect(data.data.fixedPriceSale.allocationMax).toMatch((await launchedfixedPriceSale.allocationMax()).toString())
+    expect(data.data.fixedPriceSale.tokenIn.id).toMatch(biddingToken.address)
+    expect(data.data.fixedPriceSale.tokenIn.name).toMatch(await biddingToken.name())
+    expect(data.data.fixedPriceSale.tokenIn.symbol).toMatch(await biddingToken.symbol())
+    expect(data.data.fixedPriceSale.tokenIn.decimals).toMatch((await biddingToken.decimals()).toString())
+    expect(data.data.fixedPriceSale.tokenOut.id).toMatch(fixedPriceSaleToken.address)
+    expect(data.data.fixedPriceSale.tokenOut.name).toMatch(await fixedPriceSaleToken.name())
+    expect(data.data.fixedPriceSale.tokenOut.symbol).toMatch(await fixedPriceSaleToken.symbol())
+    expect(data.data.fixedPriceSale.tokenOut.decimals).toMatch((await fixedPriceSaleToken.decimals()).toString())
+  })
+})
