7dccdc7def043506b46a6d9c70f47c2addab5770
Adam Azad <adam@adamazad.com>
2021-04-29 08:26:36 +0300

    Fix assertions

--- tests/templateLauncher.spec.ts
+++ tests/templateLauncher.spec.ts
@@ -1,92 +1,76 @@
 // Helpers
 import { mesaJestAfterEach, mesaJestBeforeEach, MesaJestBeforeEachContext } from '../jest/setup'
-import { getContractFactory, SUBGRAPH_SYNC_SECONDS, wait } from './helpers'
-import { addSaleTemplateToLauncher } from '../scripts/helpers'
-import { FixedPriceSale } from './helpers/contracts'
+import { getContractFactory, getSigners, SUBGRAPH_SYNC_SECONDS, wait } from './helpers'
+import { FixedPriceSale, FixedPriceSaleTemplate } from './helpers/contracts'
 
 // Test block
 describe('TemplateLauncher', function() {
   let mesa: MesaJestBeforeEachContext
+  let fixedPriceSaleTemplate: FixedPriceSaleTemplate
+  const templateId = 1
 
   beforeEach(async () => {
     mesa = await mesaJestBeforeEach()
+
+    const [deployer] = getSigners(mesa.provider)
+    // Register sale
+    const fixedPriceSale = (await getContractFactory('FixedPriceSale', deployer).deploy()) as FixedPriceSale
+    // Register FixedPriceSale in SaleLauncher
+    const fixedPriceSaleTemplate = (await getContractFactory(
+      'FixedPriceSaleTemplate',
+      deployer
+    ).deploy()) as FixedPriceSaleTemplate
+    // Register sale in SaleLauncher
+    await (await mesa.saleLauncher.addTemplate(fixedPriceSale.address)).wait(1)
+    // Regiter template in TemplateLauncher
+    await (await mesa.templateLauncher.addTemplate(fixedPriceSaleTemplate.address)).wait(1)
   })
 
   afterEach(async () => {
     await mesaJestAfterEach()
   })
   test('Should save new SaleTemplate', async () => {
-    const fixedPriceSaleTemplate = (await getContractFactory(
-      'FixedPriceSaleTemplate',
-      mesa.provider.getSigner(0)
-    ).deploy()) as FixedPriceSale
-    const event = await addSaleTemplateToLauncher({
-      launcher: mesa.templateLauncher,
-      saleTemplateAddress: fixedPriceSaleTemplate.address
-    })
-    await wait(SUBGRAPH_SYNC_SECONDS * 3)
+    await wait(SUBGRAPH_SYNC_SECONDS)
     const { data } = await mesa.fetchFromTheGraph(`{
-          saleTemplate (id: "${event.template}") {
+          saleTemplate (id: "${templateId}") {
+            id
             address
             factory
             name
             verified
           }
         }`)
-    expect(data.data.saleTemplate.address.toLowerCase()).toMatch(fixedPriceSaleTemplate.address)
-    expect(data.data.saleTemplate.factory.toLowerCase()).toMatch(mesa.mesaFactory.address)
-    expect(data.data.saleTemplate.name.toLowerCase()).toMatch('FixedPriceSaleTemplate')
+    expect(data.data.saleTemplate.id.toString()).toMatch('1')
+    expect(data.data.saleTemplate.address.toLowerCase()).toMatch(fixedPriceSaleTemplate.address.toLowerCase())
+    expect(data.data.saleTemplate.factory.toLowerCase()).toMatch(mesa.mesaFactory.address.toLowerCase())
+    expect(data.data.saleTemplate.name).toMatch(await fixedPriceSaleTemplate.templateName())
     expect(data.data.saleTemplate.verified).toBeFalsy()
   })
   test('Should save update SaleTemplate as verified', async () => {
-    const fixedPriceSaleTemplate = (await getContractFactory(
-      'FixedPriceSaleTemplate',
-      mesa.provider.getSigner(0)
-    ).deploy()) as FixedPriceSale
-    const event = await addSaleTemplateToLauncher({
-      launcher: mesa.templateLauncher,
-      saleTemplateAddress: fixedPriceSaleTemplate.address
-    })
-    await (await mesa.templateLauncher.verifyTemplate(event.templateId)).wait(1)
+    await (await mesa.templateLauncher.verifyTemplate(1)).wait(1)
     await wait(SUBGRAPH_SYNC_SECONDS * 2)
     const { data } = await mesa.fetchFromTheGraph(`{
-          saleTemplate (id: "${event.templateId}") {
+          saleTemplate (id: "${templateId}") {
             verified
           }
         }`)
     expect(data.data.saleTemplate.verified).toBeTruthy()
   })
   test('Should save update SaleTemplate as verified', async () => {
-    const fixedPriceSaleTemplate = (await getContractFactory(
-      'FixedPriceSaleTemplate',
-      mesa.provider.getSigner(0)
-    ).deploy()) as FixedPriceSale
-    const event = await addSaleTemplateToLauncher({
-      launcher: mesa.templateLauncher,
-      saleTemplateAddress: fixedPriceSaleTemplate.address
-    })
-    await (await mesa.templateLauncher.verifyTemplate(event.templateId)).wait(1)
+    await (await mesa.templateLauncher.verifyTemplate(templateId)).wait(1)
     await wait(SUBGRAPH_SYNC_SECONDS * 2)
     const { data } = await mesa.fetchFromTheGraph(`{
-          saleTemplate (id: "${event.templateId}") {
+          saleTemplate (id: "${templateId}") {
             verified
           }
         }`)
     expect(data.data.saleTemplate.verified).toBeTruthy()
   })
   test('Should save update SaleTemplate as verified', async () => {
-    const fixedPriceSaleTemplate = (await getContractFactory(
-      'FixedPriceSaleTemplate',
-      mesa.provider.getSigner(0)
-    ).deploy()) as FixedPriceSale
-    const event = await addSaleTemplateToLauncher({
-      launcher: mesa.templateLauncher,
-      saleTemplateAddress: fixedPriceSaleTemplate.address
-    })
-    await (await mesa.templateLauncher.removeTemplate(event.templateId)).wait(1)
+    await (await mesa.templateLauncher.removeTemplate(1)).wait(1)
     await wait(SUBGRAPH_SYNC_SECONDS * 2)
     const { data } = await mesa.fetchFromTheGraph(`{
-          saleTemplate (id: "${event.templateId}") {
+          saleTemplate (id: "${templateId}") {
             deleted
           }
         }`)
