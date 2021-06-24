f8b18e387446ef51ce1c42ecdda42174e5772f77
Adam Azad <adam@adamazad.com>
2021-06-24 17:35:36 +0200

    Add helper functions

---
+++ src/helpers/fixedPriceSale.ts
@@ -0,0 +1,66 @@
+import { Address, BigInt } from '@graphprotocol/graph-ts'
+import { FixedPriceSaleUser } from '../../generated/schema'
+
+// Predefined Auction Bid status
+export abstract class PURCHASE_STATUS {
+  static SUBMITTED: string = 'submitted'
+  static CLAIMED: string = 'claimed'
+}
+
+/**
+ * Helper function to construct the `FixedPriceSaleUser` entity IDs
+ */
+export function createFixedPriceSaleUserId(saleAddress: Address, userAddres: Address) {
+  let fixedPriceSaleUserId = saleAddress.toHexString() + '/users/' + userAddres.toHexString()
+  return fixedPriceSaleUserId
+}
+
+/**
+ * Helper function to construct the `FixedPriceSalePurchase` entity IDs
+ */
+export function createFixedPriceSalePurchaseId(saleAddress: Address, userAddres: Address, purchaseIndex: number) {
+  let fixedPriceSalePurchaseId =
+    saleAddress.toHexString() + '/purchases/' + userAddres.toHexString() + '/' + purchaseIndex.toString()
+  return fixedPriceSalePurchaseId
+}
+
+/**
+ * Creates a new FixedPriceSaleUser entity or returns an existing one
+ */
+export function createOrGetFixedPriceSaleUser(
+  saleAddress: Address,
+  userAddres: Address,
+  timestamp: BigInt
+): FixedPriceSaleUser {
+  let fixedPriceSaleUserId = createFixedPriceSaleUserId(saleAddress, userAddres)
+
+  // First, fetch or register the new user
+  let fixedPriceSaleUser = FixedPriceSaleUser.load(fixedPriceSaleUserId)
+  // Create them
+  if (fixedPriceSaleUser == null) {
+    fixedPriceSaleUser = new FixedPriceSaleUser(fixedPriceSaleUserId)
+    fixedPriceSaleUser.totalPurchases = 0
+    fixedPriceSaleUser.createdAt = timestamp
+    fixedPriceSaleUser.updatedAt = timestamp
+    fixedPriceSaleUser.save()
+  }
+
+  return fixedPriceSaleUser as FixedPriceSaleUser
+}
+
+/**
+ * Returns the total purchases
+ * @param saleAddress
+ * @param userAddres
+ * @returns
+ */
+export function getFixedPriceSaleUserTotalPurchases(fixedPriceSaleUserId: string): number {
+  // First, fetch or register the new user
+  let fixedPriceSaleUser = FixedPriceSaleUser.load(fixedPriceSaleUserId)
+
+  if (fixedPriceSaleUser == null) {
+    return 0
+  }
+
+  return fixedPriceSaleUser.totalPurchases
+}
