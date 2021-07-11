565d88f25aab9f3acbe3ac72ec5343a90d6640aa
Adam Azad <adam@adamazad.com>
2021-07-11 11:43:46 +0200

    Update event signatures

--- src/mappings/sales/fixedPriceSale.ts
+++ src/mappings/sales/fixedPriceSale.ts
@@ -8,9 +8,9 @@ import {
 } from '../../helpers/fixedPriceSale'
 import {
   FixedPriceSale as FixedPriceSaleContract,
+  NewTokenWithdraw,
   NewTokenRelease,
-  NewTokenClaim,
-  NewPurchase,
+  NewCommitment,
   SaleClosed
 } from '../../../generated/FixedPriceSale/FixedPriceSale'
 
@@ -34,7 +34,7 @@ export function handleSaleClosed(event: SaleClosed): void {
  * WIP
  * Handles `NewPurchase` when a investors buy certain amount of tokens
  */
-export function handleNewPurchase(event: NewPurchase): void {
+export function handleNewPurchase(event: NewCommitment): void {
   // Get the sale record
   let fixedPriceSale = FixedPriceSale.load(event.address.toHexString())
   if (!fixedPriceSale) {
@@ -45,7 +45,7 @@ export function handleNewPurchase(event: NewPurchase): void {
   let fixedPriceSaleContract = FixedPriceSaleContract.bind(event.address)
 
   // Get the user
-  let fixedPriceSaleUser = createOrGetFixedPriceSaleUser(event.address, event.params.buyer, event.block.timestamp)
+  let fixedPriceSaleUser = createOrGetFixedPriceSaleUser(event.address, event.params.user, event.block.timestamp)
   // Increase the total purcahses by one and save
   // Push change to FixedPriceSaleUser entity
   let newPurchaseIndex = fixedPriceSaleUser.totalPurchase + 1
@@ -53,18 +53,18 @@ export function handleNewPurchase(event: NewPurchase): void {
   // Increase the total volume for user+sale pair
   fixedPriceSaleUser.totalVolume = event.params.amount.plus(fixedPriceSaleUser.totalVolume)
   // Construct the purchase id
-  let purchaseId = createFixedPriceSalePurchaseId(event.address, event.params.buyer, newPurchaseIndex)
+  let purchaseId = createFixedPriceSalePurchaseId(event.address, event.params.user, newPurchaseIndex)
   // Create the FixedPriceSalePurchase entity
   let purchase = new FixedPriceSalePurchase(purchaseId)
   purchase.createdAt = event.block.timestamp.toI32()
   purchase.updatedAt = event.block.timestamp.toI32()
   // Update the reference
   purchase.sale = event.address.toHexString()
-  purchase.buyer = event.params.buyer
+  purchase.buyer = event.params.user
   purchase.amount = event.params.amount
   purchase.status = PURCHASE_STATUS.SUBMITTED
   // update `soldAmount` field in the sale
-  fixedPriceSale.soldAmount = fixedPriceSaleContract.tokensSold()
+  fixedPriceSale.soldAmount = fixedPriceSaleContract.remainingTokensForSale()
   // Save all entities
   purchase.save()
   fixedPriceSale.save()
@@ -74,13 +74,13 @@ export function handleNewPurchase(event: NewPurchase): void {
 /**
  * WIP
  */
-export function handleNewTokenClaim(event: NewTokenClaim): void {
+export function handleNewTokenClaim(event: NewTokenWithdraw): void {
   // Get total purchases by the investor/buyer
-  let totalPurchases = getFixedPriceSaleUserTotalPurchase(createFixedPriceSaleUserId(event.address, event.params.buyer))
+  let totalPurchases = getFixedPriceSaleUserTotalPurchase(createFixedPriceSaleUserId(event.address, event.params.user))
   // Loop through the purchases and update their status for the buyer
   for (let purchaseIndex = 1; purchaseIndex <= totalPurchases; purchaseIndex++) {
     let purchase = FixedPriceSalePurchase.load(
-      createFixedPriceSalePurchaseId(event.address, event.params.buyer, purchaseIndex)
+      createFixedPriceSalePurchaseId(event.address, event.params.user, purchaseIndex)
     )
 
     if (purchase) {
@@ -95,7 +95,6 @@ export function handleNewTokenClaim(event: NewTokenClaim): void {
  */
 export function handleNewTokenRelease(event: NewTokenRelease): void {
   // tokens are swapped; sent to investors
-  //
   let fixedPriceSale = FixedPriceSale.load(event.address.toHexString())
   if (!fixedPriceSale) {
     return
