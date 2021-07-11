c28d2e547e77ed4314583b66612ce857f21e6899
Adam Azad <adam@adamazad.com>
2021-07-11 11:01:57 +0200

    Add FixedPriceSaleSaleInfo to map SaleInfo struct

--- src/helpers/fixedPriceSale.ts
+++ src/helpers/fixedPriceSale.ts
@@ -1,4 +1,5 @@
 import { Address, BigInt } from '@graphprotocol/graph-ts'
+import { FixedPriceSale__saleInfoResult } from '../../generated/FixedPriceSale/FixedPriceSale'
 // Schema
 import { FixedPriceSaleUser } from '../../generated/schema'
 
@@ -77,4 +78,55 @@ export function getFixedPriceSaleUserTotalPurchase(fixedPriceSaleUserId: string)
   }
 
   return fixedPriceSaleUser.totalPurchase
+}
+
+/**
+ * Maps the graph array to named variables for readibility
+ * The contract uses struct which the graph resolve to array-like shape with value-[index]
+ */
+export class FixedPriceSaleSaleInfo {
+  /*
+    struct SaleInfo {
+        0 = IERC20 tokenIn;
+        1 = IERC20 tokenOut;
+        2 = uint256 tokenPrice;
+        3 = uint256 tokensForSale;
+        4 = uint256 startDate;
+        5 = uint256 endDate;
+        6 = uint256 minCommitment;
+        7 = uint256 maxCommitment;
+        8 = uint256 minRaise;
+        9 = bool hasParticipantList;
+        10 = address participantList;
+    }*/
+
+  readonly tokenIn: Address
+  readonly tokenOut: Address
+  readonly tokenPrice: BigInt
+  readonly tokensForSale: BigInt
+  readonly startDate: BigInt
+  readonly endDate: BigInt
+  readonly minCommitment: BigInt
+  readonly maxCommitment: BigInt
+  readonly minRaise: BigInt
+  readonly hasParticipantList: boolean
+  readonly participantList: Address
+
+  private constructor(saleInfo: FixedPriceSale__saleInfoResult) {
+    this.tokenIn = saleInfo.value0
+    this.tokenOut = saleInfo.value1
+    this.tokenPrice = saleInfo.value2
+    this.tokensForSale = saleInfo.value3
+    this.startDate = saleInfo.value4
+    this.endDate = saleInfo.value5
+    this.minCommitment = saleInfo.value6
+    this.maxCommitment = saleInfo.value7
+    this.minRaise = saleInfo.value8
+    this.hasParticipantList = saleInfo.value9
+    this.participantList = saleInfo.value10
+  }
+
+  static fromResult(saleInfo: FixedPriceSale__saleInfoResult): FixedPriceSaleSaleInfo {
+    return new FixedPriceSaleSaleInfo(saleInfo)
+  }
 }
