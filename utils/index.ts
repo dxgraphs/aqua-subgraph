31e4c9db616c2a0e13e53542f99a7606a3fd107c
Adam Azad <adam@adamazad.com>
2021-07-31 00:34:25 +0200

    Add logger

--- utils/index.ts
+++ utils/index.ts
@@ -2,6 +2,7 @@
 export * from './constants'
 export * from './contracts'
 export * from './encoders'
+export * from './logger'
 export * from './graph'
 export * from './types'
 export * from './time'
