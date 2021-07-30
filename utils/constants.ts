1363ea834b887d49c78067667267e5057e420b87
Adam Azad <adam@adamazad.com>
2021-07-31 00:38:47 +0200

    Refactor constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,7 +2,9 @@ export const SUBGRAPH_SYNC_SECONDS = 1500
 export const ETH_ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 // The Graph GraphQL endpoint
-export const GRAPHQL_ENDPOINT = 'http://localhost:8000/subgraphs/name/adamazad/aqua'
+export const SUBGRAPH_NAME = 'adamazad/aqua'
+export const GRAPHQL_ENDPOINT = `http://localhost:8000/subgraphs/name/${SUBGRAPH_NAME}`
+export const GRAPH_ADMIN_ENDPOINT = 'http://localhost:8020'
 
 // Ganache EVM endpoint
 export const EVM_ENDPOINT = 'http://localhost:8545'
