f741b61bc39b9548bd965c9b8f63b5be63f3544d
Adam Azad <adam@adamazad.com>
2021-07-30 12:49:43 +0200

    Map AmountsUpdated and ListInitialized

--- src/mappings/participantList.ts
+++ src/mappings/participantList.ts
@@ -1,37 +1,60 @@
+import { ByteArray, Bytes, log } from '@graphprotocol/graph-ts'
 import { ListInitialized, AmountsUpdated } from '../../generated/ParticipantList/ParticipantList'
 import { ParticipantList, Participant } from '../../generated/schema'
 
 /**
  * @todo Redesign this naming to:
- * Add participant
- * Remove participant
- * @param event
+ * 1. Add participant
+ * 2. Remove participant
+ *
+ * @todo Update AmountsUpdated. See https://github.com/cryptonative-ch/mesa-smartcontracts/issues/112
+ *
  */
-export function handleAmountsUpdated(event: AmountsUpdated): void {}
+export function handleAmountsUpdated(event: AmountsUpdated): void {
+  // Get the parent ParticipantList entity
+  let participantList = new ParticipantList(event.address.toHexString())
+  if (!participantList) {
+    return
+  }
+  // Create and save Participant entity
+  let participant = new Participant(event.address.toHexString() + '/participants/' + event.address.toHexString())
+  participant.createdAt = event.block.timestamp.toI32()
+  participant.updatedAt = event.block.timestamp.toI32()
+  participant.address = event.params.account
+  /**
+   */
+  participant.amount = event.params.amounts
+  participant.save()
+  // Push participant to list
+  // Linting will say object is undefined, but ignore it
+  // @ts-ignore
+  participantList.participants.push(participant.id)
+  participantList.save()
+}
 
 /**
  * Pushes list of managers (addresses) to the ParticipantList entity
- * @param event
  */
 export function handleListInitialized(event: ListInitialized): void {
   let participantList = new ParticipantList(event.address.toHexString())
   participantList.createdAt = event.block.timestamp.toI32()
   participantList.updatedAt = event.block.timestamp.toI32()
-  // Map each participant address into a entity
-  // then keep a reference for ParticipantList.participants
-  let participants: string[] = []
-  // Create a Participant for each address
-  event.params.managers.forEach(manager => {
-    // ID format: <ParticipantListAddress>/participants/<ManagerAddress>
-    let participantId = event.address.toHexString() + '/participants/' + manager.toHexString()
-    participants.push(participantId)
-    let participant = new Participant(participantId)
-    participant.createdAt = event.block.timestamp.toI32()
-    participant.updatedAt = event.block.timestamp.toI32()
-    participant.address = manager
-    participant.save()
-  })
-  // Update the reference
-  participantList.participants = participants
+  // Start with a default list
+  let managers: Bytes[] = []
+  // Clousure isn't supported here, hence `Array.forEach` does not work
+  // the seoncd best option without too many math was for loop, with a catch
+  let manager = event.params.managers.shift() || null
+  for (let i = 0; i < event.params.managers.length; i++) {
+    if (manager != null) {
+      log.debug('Manager address is {}', [manager.toHexString()])
+      managers.push(manager)
+      // Move cursor
+      manager = event.params.managers.shift() || null
+    }
+  }
+  // Add list of managers
+  participantList.managers = managers
+  // Start with an empty list
+  participantList.participants = []
   participantList.save()
 }
